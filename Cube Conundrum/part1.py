f669e93ab6e19b49145d595026e864466e45156d
Sapj0 <aschlumb@gmail.com>
2023-12-14 11:23:18 +0100

    Test avec vsc

---
+++ Cube Conundrum/part1.py
@@ -0,0 +1,10 @@
+
+
+filename = "./input/puzzle.txt"
+
+with open(filename, "r") as f:
+    content = f.readlines()
+    
+print(content)
+
+
--- Trebuchet/part2.py
+++ Trebuchet/part2.py
@@ -1,10 +1,11 @@
 import re
 from pathlib import Path
 
-path = Path(__file__).parent / "../input/Trebuchet/puzzle.txt"
+# path = Path(__file__).parent / "../input/Trebuchet/puzzle.txt"
+filename = "./input/puzzle.txt"
 
 # Open the file and read its content.
-with path.open() as f:
+with open(filename, "r") as f:
     content = f.readlines()
 
 somme = 0
